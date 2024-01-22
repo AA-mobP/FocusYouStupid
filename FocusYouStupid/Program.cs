1a8fab8adc8e25a63f8e3a977e01856957454a4c
abdelhady <bodyyousef169@gmail.com>
2024-01-22 22:30:18 +0200

    Add project files.

---
+++ FocusYouStupid/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Timers;
+using System.Threading;
+using System.Windows;
+using Microsoft.VisualBasic;
+
+namespace FocusYouStupid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
