836d5b4ab3ecdde4e8253152ef16edb19a2b92c3
abdelhady <bodyyousef169@gmail.com>
2024-01-22 22:35:40 +0200

    Creating the First Base of the Program

--- FocusYouStupid/Program.cs
+++ FocusYouStupid/Program.cs
@@ -10,8 +10,35 @@ namespace FocusYouStupid
     {
         static void Main(string[] args)
         {
+            string Prompt;
+
+            System.Timers.Timer timer = new System.Timers.Timer(30000);
+            timer.Elapsed += Timer_Elapsed;
+
+            startLoop:
+            Prompt = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(Prompt))
+            {
+                Console.WriteLine($"You Should type 'run' or 'stop' to run, stop the program");
+            }
+            else
+            {
+                if (Prompt.ToLower() == "run")
+                    timer.Start();
+                else if (Prompt.ToLower() == "stop")
+                    timer.Stop();
+                else
+                    Console.WriteLine($"{Prompt} is not a vaild command");
+            }
             
+
+            goto startLoop;
         }
 
+        private static void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            Process.Start("I:\\download\\Documents\\AutoHotkey\\FocusYouStupid.ahk");
+        }
     }
 }
